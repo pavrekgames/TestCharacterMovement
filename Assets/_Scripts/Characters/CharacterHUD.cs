4be7c92a8b5d8d4b46e38b704dae2b6017065d8d
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-01-07 18:38:22 +0100

    Create and implement character HUD base

---
+++ Assets/_Scripts/Characters/CharacterHUD.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace TestCharactersMovement.CharactersSystem
+{
+    public class CharacterHUD : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerExitHandler
+    {
+
+        [Header("General")]
+        [SerializeField] private Character character;
+        [SerializeField] private Image characterImage;
+        [SerializeField] private bool isActive = false;
+
+        [Header("Colors")]
+        [SerializeField] private Color selectColor;
+        [SerializeField] private Color deselectColor;
+        [SerializeField] private Color hoverColor;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if(!isActive)
+            {
+                characterImage.color = hoverColor;
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (!isActive)
+            {
+                SelectCharacter();
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (!isActive)
+            {
+                characterImage.color = deselectColor;
+            }
+        }
+
+        public void SelectCharacter()
+        {
+            characterImage.color = selectColor;
+            isActive = true;
+        }
+
+        public void DeselectCharacter()
+        {
+            characterImage.color = deselectColor;
+            isActive = false;
+        }
+
+
+    }
+}
+
+
