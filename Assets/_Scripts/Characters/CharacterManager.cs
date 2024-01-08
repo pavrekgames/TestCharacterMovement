61c6ae64524c3b6ff0048a93632eead88f866612
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2024-01-08 11:17:55 +0100

    Implement setting target for character based on mouse position on the map

--- Assets/_Scripts/Characters/CharacterManager.cs
+++ Assets/_Scripts/Characters/CharacterManager.cs
@@ -6,13 +6,35 @@ namespace TestCharactersMovement.CharactersSystem
 {
     public class CharacterManager : MonoBehaviour
     {
-
+        [Header("Characters")]
         [SerializeField] private List<Character> allCharacters = new List<Character>();
         [SerializeField] private Character currentCharacter;
 
-        private void Start ()
+        [Header("Character Target")]
+        [SerializeField] private Camera mainCamera;
+        [SerializeField] private Vector3 screenPosition;
+        [SerializeField] private Vector3 target;
+        [SerializeField] private float hitDistance = 100;
+        [SerializeField] private LayerMask layerMask;
+
+        private void Initialize()
         {
+            mainCamera = Camera.main;
+        }
+
+        private void Start()
+        {
+            Initialize();
+
             CharacterHUD.OnCharacterSelected += SelectCharacter;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SetTarget();
+            }
         }
 
         public void SelectCharacter(Character character)
@@ -32,7 +54,15 @@ namespace TestCharactersMovement.CharactersSystem
 
         private void SetTarget()
         {
+            screenPosition = Input.mousePosition;
 
+            Ray ray = mainCamera.ScreenPointToRay(screenPosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, hitDistance))
+            {
+                target = hitInfo.point;
+                currentCharacter.transform.position = target;
+            }
         }
 
         private void MoveCharacter()
